20a0c25b18db80a47f98f5d8ccb467ff62659689
ItsEthanH <89361954+ItsEthanH@users.noreply.github.com>
2023-12-14 12:17:23 +0000

    more comments in eleventy config file

--- .eleventy.js
+++ .eleventy.js
@@ -2,44 +2,58 @@
 const pluginEleventyNavigation = require("@11ty/eleventy-navigation");
 const pluginMinifier = require("@sherby/eleventy-plugin-files-minifier");
 const pluginSitemap = require("@quasibit/eleventy-plugin-sitemap");
+
 const configCssExtension = require("./src/config/cssExtension");
 const configSitemap = require("./src/config/sitemap");
 const configServer = require("./src/config/server");
+
 const filterPostDate = require("./src/config/postDate");
 
 module.exports = function (eleventyConfig) {
-    // setting up CSS as an eleventy template extension (not supported in base 11ty)
+    // EXTENSIONS - Recognising non-default languages as templates
+    // Setting up CSS files to be recognised as a template language, and can be passed through eleventy. This allows our minifier to read CSS files and minify them
     eleventyConfig.addTemplateFormats("css");
     eleventyConfig.addExtension("css", configCssExtension);
+    // END EXTENSIONS
 
-    // adds the official eleventy navigation plugin for a scalable navigation
+    // PLUGINS - Adds additional eleventy functionality
+    // Sets up the eleventy navigation plugin for a scalable navigation as used in _includes/components/header.html
+    // https://github.com/11ty/eleventy-navigation
     eleventyConfig.addPlugin(pluginEleventyNavigation);
 
-    // adds a minifier when `npm run build` is run, so code is only minified in production
+    // Automatically generate a sitemap, using the domain in _data/client.json
+    // https://www.npmjs.com/package/@quasibit/eleventy-plugin-sitemap
+    eleventyConfig.addPlugin(pluginSitemap, configSitemap);
+
+    // When in production ("npm run build" is ran), minify all HTML, CSS, JSON, XML, XSL and webmanifest files.
+    // https://github.com/benjaminrancourt/eleventy-plugin-files-minifier
     if (configServer.isProduction) {
         eleventyConfig.addPlugin(pluginMinifier);
     }
+    // END PLUGINS
 
-    eleventyConfig.addPlugin(pluginSitemap, configSitemap);
-
+    // SERVER - Set how the eleventy dev server is run, using the options from https://www.11ty.dev/docs/dev-server/
     eleventyConfig.setServerOptions(configServer);
+    // END SERVER
 
-    // passthroughs allow non-template (html, njk) files to be put into /public
-    // here, we individually specify what folders are passed through. this prevents sass files from being deployed unnecessarily
+    // PASSTHROUGHS - "Pass through" source files to /public, without being processed by eleventy
+    // Individually specify what asset folders are passed through. SASS is processed by it's compiler into ./src and passed through as a template for minification. JS is bundled separately
     eleventyConfig.addPassthroughCopy("./src/assets/css");
     eleventyConfig.addPassthroughCopy("./src/assets/favicons");
     eleventyConfig.addPassthroughCopy("./src/assets/fonts");
     eleventyConfig.addPassthroughCopy("./src/assets/images");
     eleventyConfig.addPassthroughCopy("./src/assets/svgs");
 
-    // other important files are passed through. this allows the CMS, redirects, robots.txt and sitemap to be present in the deployed project
+    // Other required folders are passed through
     eleventyConfig.addPassthroughCopy("./src/admin");
     eleventyConfig.addPassthroughCopy("./src/_redirects");
     eleventyConfig.addPassthroughCopy({ "./src/robots.txt": "/robots.txt" });
+    // END PASSTHROUGHS
 
-    // normally, 11ty will render dates on blog posts in full JSDate format (Fri Dec 02 18:00:00 GMT-0600)
-    // this filter allows dates to be converted into a normal, locale format. view the docs to learn more (https://moment.github.io/luxon/api-docs/index.html#datetime)
+    // FILTERS - Modify data in template files at build time
+    // Converts dates from JSDate format (Fri Dec 02 18:00:00 GMT-0600) to a locale format. More info in docs - https://moment.github.io/luxon/api-docs/index.html#datetime
     eleventyConfig.addFilter("postDate", filterPostDate);
+    // END FILTERS
 
     return {
         dir: {
