c491462feb7cb949b3c6db9d3b41d5c567d797c7
NevaMasquerade <hutchins.tianna@gmail.com>
2025-03-14 22:10:14 -0400

    Initial setup
    Set colors and fonts and added homepage sections, logo, favicon.

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -109,3 +109,14 @@ document.addEventListener("keydown", (event) => {
         toggleMenu();
     }
 });
+
+// tertiary nav toggle code
+const tertiaryDrop = Array.from(document.querySelectorAll('#cs-navigation .cs-drop3-main'));
+
+for (const item of tertiaryDrop) {
+    item.addEventListener('click', (e) => {
+        e.stopPropagation();
+        item.classList.toggle('drop3-active');
+    });
+}
+                                
