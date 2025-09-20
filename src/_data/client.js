6811af2739c81be297fb3edf433869bb1169fb2d
StevenManner <Stevenkmanner@gmail.com>
2025-09-19 19:17:56 -1000

    1

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,23 +1,19 @@
 module.exports = {
-    name: "Code Stitch Web Designs",
-    email: "help@codestitch.app",
-    phoneForTel: "555-779-4407",
+    name: "Kai Web Services",
+    email: "info@kaiwebservices.com",
+    phoneForTel: "808-365-8530",
     phoneFormatted: "(555) 779-4407",
     address: {
-        lineOne: "First Address Line",
-        lineTwo: "Second Address Line",
-        city: "Denver",
-        state: "CO",
-        zip: "80206",
-        country: "US",
-        mapLink: "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6",
+        lineOne: "Hawaii County, HI",
+       
+      
     },
     socials: {
-        facebook: "https://www.facebook.com/",
-        instagram: "https://www.instagram.com/",
+        facebook: "https://www.facebook.com/kaiwebservices",
+        instagram: "https://www.instagram.com/kaiwebservices",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    domain: "https://www.example.com",
+    domain: "https://kaiwebservices.com",
     // Passing the isProduction variable for use in HTML templates
     isProduction: process.env.ELEVENTY_ENV === "PROD",
 };
