270c994ce0b89a7be0525c02fb53f9c6c6e31a08
lockswebsolutions <lockswebsolutions@gmail.com>
2025-10-23 16:18:02 -0400

    WCAG accessible navigation menu and dropdown

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -1,102 +1,124 @@
 // Select DOM elements
 const bodyElement = document.querySelector("body");
-const navbarMenu = document.querySelector("#cs-navigation");
-const hamburgerMenu = document.querySelector("#cs-navigation .cs-toggle");
+const navbar = document.querySelector("#cs-navigation");
+const navbarMenu = document.getElementById("cs-expanded");
+const hamburgerToggle = document.querySelector("#cs-navigation .cs-toggle");
 
-// Function to toggle the aria-expanded attribute
+const maxWidthMediaQuery = window.matchMedia("(max-width: 63.9375rem)");
+
+// Function to toggle the aria-expanded attribute - utility function
 function toggleAriaExpanded(element) {
-    const isExpanded = element.getAttribute("aria-expanded");
-    element.setAttribute("aria-expanded", isExpanded === "false" ? "true" : "false");
+  const isExpanded = element.getAttribute("aria-expanded");
+  element.setAttribute(
+    "aria-expanded",
+    isExpanded === "false" ? "true" : "false"
+  );
 }
 
-// Function to toggle the menu open or closed
-function toggleMenu() {
-    hamburgerMenu.classList.toggle("cs-active");
-    navbarMenu.classList.toggle("cs-active");
-    bodyElement.classList.toggle("cs-open");
-    toggleAriaExpanded(hamburgerMenu);
+// Function to toggle let screen reader know it is hidden from DOM. aira-hidden - utility function
+function toggleAriaHidden(element) {
+  const isHidden = element.getAttribute("aria-hidden");
+  element.setAttribute("aria-hidden", isHidden === "false" ? "true" : "false");
 }
+
+// Function to toggle the navbar menu open or closed
+function toggleMenu() {
+  hamburgerToggle.classList.toggle("cs-active");
+  navbar.classList.toggle("cs-active");
+  bodyElement.classList.toggle("cs-open");
+
+  toggleAriaHidden(navbarMenu);
+  toggleAriaExpanded(hamburgerToggle);
+}
+
+// Find current screen width of window size on document load so we know whether or not to added aria-hidden and aira-expand with the navbar menu
+document.addEventListener("DOMContentLoaded", function () {
+  // we are on desktop open the menu
+  if (!maxWidthMediaQuery.matches) {
+    toggleAriaHidden(navbarMenu);
+    toggleAriaExpanded(hamburgerToggle);
+  }
+});
+
+// Toggle at breakpoint becuase we automatically open and close the menu depending on screen size
+maxWidthMediaQuery.addEventListener("change", function () {
+  toggleAriaHidden(navbarMenu);
+  toggleAriaExpanded(hamburgerToggle);
+});
 
 // Add click event listener to the hamburger menu
-hamburgerMenu.addEventListener("click", toggleMenu);
+hamburgerToggle.addEventListener("click", toggleMenu);
 
 // Add click event listener to the navbar menu to handle clicks on the pseudo-element
-navbarMenu.addEventListener("click", function (event) {
-    if (event.target === navbarMenu && navbarMenu.classList.contains("cs-active")) {
-        toggleMenu();
-    }
+navbar.addEventListener("click", function (event) {
+  if (event.target === navbar && navbar.classList.contains("cs-active")) {
+    toggleMenu();
+  }
 });
 
 // Function to handle dropdown toggle
 function toggleDropdown(element) {
-    element.classList.toggle("cs-active");
-    const dropdownButton = element.querySelector(".cs-dropdown-button");
-    if (dropdownButton) {
-        toggleAriaExpanded(dropdownButton);
-    }
+  const dropdownButton = element.querySelector(".cs-dropdown-button");
+  const dropdownMenu = element.querySelector(
+    ".cs-dropdown-button ~ .cs-drop-ul"
+  );
+
+  toggleAriaHidden(dropdownMenu);
+  toggleAriaExpanded(dropdownButton);
+
+  element.classList.toggle("cs-active");
 }
 
 // Add event listeners to each dropdown element for accessibility
 const dropdownElements = document.querySelectorAll(".cs-dropdown");
-dropdownElements.forEach(element => {
-    let escapePressed = false;
-
-    element.addEventListener("focusout", function (event) {
-        // Skip the focusout logic if escape was pressed
-        if (escapePressed) {
-            escapePressed = false;
-            return;
-        }
-
-        // If the focus has moved outside the dropdown, remove the active class from the dropdown 
-        if (!element.contains(event.relatedTarget)) {
-            element.classList.remove("cs-active");
-            const dropdownButton = element.querySelector(".cs-dropdown-button");
-
-            if (dropdownButton) {
-                toggleAriaExpanded(dropdownButton);
-            }
-        }
-    });
-
-    element.addEventListener("keydown", function (event) {
-        if (element.classList.contains("cs-active")) {
-            event.stopPropagation();
-        }
-
-        // Pressing Enter or Space will toggle the dropdown and adjust the aria-expanded attribute
-        if (event.key === "Enter" || event.key === " ") {
-            event.preventDefault();
-            toggleDropdown(element);
-        }
-
-        // Pressing Escape will remove the active class from the dropdown. The stopPropagation above will stop the hamburger menu from closing
-        if (event.key === "Escape") {
-            escapePressed = true;
-            toggleDropdown(element);
-        }
-    });
-
-    // Handles dropdown menus on mobile - the matching media query (max-width: 63.9375rem) is necessary so that clicking the dropdown button on desktop does not add the active class and thus interfere with the hover state
-    const maxWidthMediaQuery = window.matchMedia("(max-width: 63.9375rem)");
-    if (maxWidthMediaQuery.matches) {
-        element.addEventListener("click", () => toggleDropdown(element));
+dropdownElements.forEach((element) => {
+  element.addEventListener("focusout", function (event) {
+    // if its active and we exit outside of focus close dropdown menu
+    if (!element.contains(event.relatedTarget)) {
+      if (element.classList.contains("cs-active")) {
+        toggleDropdown(element);
+      }
     }
-});
+  });
 
-// Pressing Enter will redirect to the href
-const dropdownLinks = document.querySelectorAll(".cs-drop-li > .cs-li-link");
-dropdownLinks.forEach(link => {
-    link.addEventListener("keydown", function (event) {
-        if (event.key === "Enter") {
-            window.location.href = this.href;
-        }
+  // we are no longer allowing click events to open the menu as we are on desktop. We only allow hover and keyboard navigation
+  element.addEventListener("keydown", function (event) {
+    // prevents hamburger menu from closing
+    if (element.classList.contains("cs-active")) {
+      event.stopPropagation();
+    }
+
+    // Pressing Enter or Space will toggle the dropdown and adjust the aria-expanded attribute
+    if (event.key === "Enter" || event.key === " ") {
+      event.preventDefault();
+      toggleDropdown(element);
+    }
+
+    // Pressing Escape will remove the active class from the dropdown. The stopPropagation above will stop the hamburger menu from closing
+    if (event.key === "Escape") {
+      toggleDropdown(element);
+      // jump focus back to where it was before we close menu or browser can't find focus as element does not have a tab index
+      const button = element.querySelector(".cs-dropdown-button");
+      button.focus();
+    }
+  });
+
+  // Handles dropdown menus on mobile - the matching media query (max-width: 63.9375rem) is necessary so that clicking the dropdown button on desktop does not add the active class and thus interfere with the hover state
+  const maxWidthMediaQuery = window.matchMedia("(max-width: 63.9375rem)");
+  if (maxWidthMediaQuery.matches) {
+    element.addEventListener("click", function () {
+      toggleDropdown(element);
     });
+  }
 });
 
 // If you press Escape and the hamburger menu is open, close it
 document.addEventListener("keydown", (event) => {
-    if (event.key === "Escape" && hamburgerMenu.classList.contains("cs-active")) {
-        toggleMenu();
-    }
+  if (
+    event.key === "Escape" &&
+    hamburgerToggle.classList.contains("cs-active")
+  ) {
+    toggleMenu();
+    hamburgerToggle.focus();
+  }
 });
